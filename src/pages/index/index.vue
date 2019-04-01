5ac2da38d40cfe38ce1a156833076189f8bc35c2
huaisun <420007900@qq.com>
2019-04-01 20:56:29 +0800

    演示

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,10 @@
 <template>
   <van-cell-group>
     <van-field placeholder="请输入快件手机尾号" label="手机尾号" left-icon="phone" :border="false" :value="form.phone"></van-field>
-    <van-field value="" label="快递公司" left-icon="wap-home" :border="false" :value="form.company"
+    <van-field value="" label="快递公司" left-icon="wap-home" :border="false" :value="form.company" readonly
                @click="activeSelect"></van-field>
-    <van-field label="收件日期" left-icon="calender-o" :border="false" :value="form.date"></van-field>
+    <van-field label="收件日期" left-icon="calender-o" :border="false" :value="form.currentDate"
+               @click="activeDate"></van-field>
     <van-field label="件数" left-icon="chart-trending-o" :border="false" :value="form.count"></van-field>
     <van-field label="收件重量" left-icon="bag-o" :border="false" :value="form.weight"></van-field>
     <van-button class="confirm-order" type="info" size="large">下单</van-button>
@@ -12,6 +13,10 @@
       <van-picker show-toolbar :columns="columns" @confirm="onConfirm" @cancel="show = false"></van-picker>
     </van-popup>
     <!--收件日期下拉框-快捷选择-->
+    <van-popup :show="dateshow" position="bottom" overlay>
+      <van-datetime-picker :value="currentDate" type="date" :min-date="minDate" @confirm="onConfirmDate"
+                           @cancel="dateshow = false"/>
+    </van-popup>
     <!--件数下拉框-快捷选择-->
     <!--收件重量-快捷选择-->
   </van-cell-group>
@@ -27,12 +32,15 @@
         form: {
           phone: "",
           company: "韵达快递",
-          date: "2019-3-31",
+          currentDate: new Date(),
           count: "1",
           weight: "(0kg,5kg]"
         },
-        //是否显示
+        currentDate: new Date(),
+        //是否显示公司
         show: false,
+        //是否显示日期
+        dateshow: false,
         //选择下拉值
         columns: ["韵达快递", "申通快递", "圆通快递", "天天快递", "顺丰快递", "德邦快递", "EMS"]
       };
@@ -43,6 +51,18 @@
     },
 
     methods: {
+      //选中日期
+      onConfirmDate(value) {
+        console.log(new Date(value.timestamp));
+      },
+      //激活日期选择
+      activeDate() {
+        this.dateshow = true;
+      },
+      //
+      onInput() {
+
+      },
       //激活选择菜单快递公司
       activeSelect() {
         this.show = true;
@@ -53,8 +73,6 @@
         this.form.company = event.target.value;
         this.show = false;
       }
-
-
     },
 
     created() {
