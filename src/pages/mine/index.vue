2dbb9120aa8238eca197c26f5a7f226baba2ca53
huaisun <420007900@qq.com>
2019-04-09 09:01:22 +0800

    地址栏未完成

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,24 +1,28 @@
 <template>
-  <div class="div">
+  <div>
     <div class="img-photo-class">
       <div class="img-class">
         <img src='/static/images/user.png' alt="" class="photo-class">
       </div>
     </div>
-    <div class="div2">
-      <div class="div3">
-        <text>云跑红包</text>
-      </div>
-      <div class="div4">
-        <text>我的收货地址</text>
-      </div>
-      <div class="div5">
-        <text>客服电话：10086</text>
-      </div>
-      <div class="div6">
-        <text>退出账号</text>
-      </div>
-    </div>
+    <van-cell-group>
+      <van-cell title="云跑红包" icon="point-gift-o" is-link @click=""></van-cell>
+      <van-cell title="我的收货地址" icon="location-o" is-link @click="selectAddress"></van-cell>
+      <van-cell title="客服电话" icon="phone-o" is-link value="10086" @click=""></van-cell>
+      <van-cell title="退出账号" icon="smile-o" is-link @click=""></van-cell>
+
+    </van-cell-group>
+    <van-popup :show="addressShow" position="right" class="right-popup">
+      <van-icon name="arrow-left" @click="addressShow = false" class="back-icon"></van-icon>
+      <van-radio-group :value="1" @change="onChange">
+        <van-cell-group>
+          <van-cell title="张三, 13000000" label="浙江省杭州市西湖区文三路138号东方通信大厦7楼501室">
+            <van-icon slot="right-icon" name="edit" class="custom-icon"></van-icon>
+          </van-cell>
+        </van-cell-group>
+      </van-radio-group>
+    </van-popup>
+
   </div>
 </template>
 
@@ -27,9 +31,15 @@
   export default {
     name: "index",
     data: () => {
-      return {};
+      return {
+        addressShow: false
+      };
     },
-    methods: {},
+    methods: {
+      selectAddress() {
+        this.addressShow = true;
+      }
+    },
     created() {
 
     }
@@ -54,6 +64,12 @@
     text-align: center;
   }
 
-  .div {
+  .right-popup .van-popup--right {
+    height: 100%;
+    width: 100%;
+  }
+
+  .back-icon {
+    margin-left: 10px;
   }
 </style>
